ea4eb60fe4cb39b708e9456ea9401699f1fc667f
DenisOuma <dennisouma38@gmail.com>
2022-07-17 21:14:45 +0300

    totalproduction function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,74 +1,57 @@
-const dashboard = document.getElementById("data-renderd");
-const information = document.getElementById("data-renderd");
-function dashboardPage() {
-	dashboard.innerHTML = `<div class="dashboard-section">
-    <h1>This week Best Breed</h1>
-    <div class="card-cows">
-        <div class="breed-card">
-            <div class="cow-img">
-                <img src="./assets/images/Ayrshire.jpg" alt="" />
-            </div>
-            <div class="container-card">
-                <div class="empty-card"></div>
-                <div class="text-element">
-                    <div class="icons-elements">
-                        <span class="material-symbols-rounded">
-                            arrow_drop_up
-                        </span>
-                        <p><span> 40%</span> Litres</p>
-                    </div>
-                    <p>Ayrshire</p>
-                    <h6>Health Status: <span>Healthy</span></h6>
-                </div>
-            </div>
-        </div>
-        <div class="breed-card">
-            <div class="cow-img">
-                <img src="./assets/images/Jersey.jpg" alt="" />
-            </div>
-            <div class="container-card">
-                <div class="empty-card"></div>
-                <div class="text-element">
-                    <div class="icons-elements">
-                        <span class="material-symbols-rounded">
-                            arrow_drop_up
-                        </span>
-                        <p><span> 16%</span> Litres</p>
-                    </div>
-                    <p>Jersey</p>
-                    <h6>Health Status: <span>Healthy</span></h6>
-                </div>
-            </div>
-        </div>
-        <div class="breed-card">
-            <div class="cow-img">
-                <img src="./assets/images/Brown-Swiss.jpg" alt="" />
-            </div>
-            <div class="container-card">
-                <div class="empty-card"></div>
-                <div class="text-element">
-                    <div class="icons-elements">
-                        <span class="material-symbols-rounded">
-                            arrow_drop_up
-                        </span>
-                        <p><span> 30%</span> Litres</p>
-                    </div>
-                    <p>Brown-Swiss</p>
-                    <h6>Health Status: <span>Healthy</span></h6>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`;
-}
+const sheds = { A: 510, B: 308, C: 486, D: 572 };
 
 function totalProduction() {
-	information.innerHTML = `<strong>Welcome to our company</strong><button onclick="anotheventPage()">click</button><div id="test"></div>`;
+	printProductionToConsole();
+
+	populateUITable();
+}
+function totalResult() {
+	let sum = 0;
+	Object.keys(sheds).forEach((item) => (sum += sheds[item]));
+	return sum;
 }
 
-function anotheventPage() {
-	console.log("Hello");
-	const evntlisten = document.getElementById("test");
-	evntlisten.innerHTML = `<strong> Hello World 3</strong>`;
-	information.appendChild(dashboard);
+function populateUITable() {
+	const tableContainer = document.getElementById("table-container");
+	const prodTotalContainer = document.getElementById("prod-total");
+	let tableString = `	<table id="">
+    <tr>
+        <th>Shed Letter</th>
+
+        <th>Numbers of Litres</th>
+
+        <th>Duration</th>
+    </tr>`;
+	Object.keys(sheds).forEach(
+		(item) =>
+			(tableString += `<tr>
+        <td>${item}</td>
+        <td>${sheds[item]}</td>
+        <td>A day</td>
+    </tr>`)
+	);
+	tableString += "</table>";
+	tableContainer.innerHTML = tableString;
+	prodTotalContainer.innerText = totalResult();
 }
+
+function printProductionToConsole() {
+	let prefix = "Your production in Shed ";
+	let postFix = " litres per day";
+
+	Object.keys(sheds).forEach((key) =>
+		console.log(prefix + sheds[key] + postFix)
+	);
+
+	console.log("The total production is " + totalResult() + postFix);
+}
+// function submitInputs() {
+// 	let BuyingPrice = document.getElementById("buying").value;
+// 	let numberOfDays = document.getElementById("days").value;
+// 	// console.log([BuyingPrice, numberOfDays]);
+// 	return [BuyingPrice, numberOfDays];
+// }
+
+// let myValues = submitInputs();
+
+// console.log(myValues);
