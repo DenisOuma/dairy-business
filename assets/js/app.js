ab643f1cb08db781ab826c1955d4e1cc68cfdbf6
DenisOuma <dennisouma38@gmail.com>
2022-07-17 16:57:09 +0300

    Finished working on the dashbaoard section design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,65 @@
 const dashboard = document.getElementById("data-renderd");
 const information = document.getElementById("data-renderd");
 function dashboardPage() {
-	dashboard.innerHTML = "<strong> Hello World 2</strong>";
+	dashboard.innerHTML = `<div class="dashboard-section">
+    <h1>This week Best Breed</h1>
+    <div class="card-cows">
+        <div class="breed-card">
+            <div class="cow-img">
+                <img src="./assets/images/Ayrshire.jpg" alt="" />
+            </div>
+            <div class="container-card">
+                <div class="empty-card"></div>
+                <div class="text-element">
+                    <div class="icons-elements">
+                        <span class="material-symbols-rounded">
+                            arrow_drop_up
+                        </span>
+                        <p><span> 16%</span> Litres</p>
+                    </div>
+                    <p>Ayrshire</p>
+                    <h6>Health Status: <span>Healthy</span></h6>
+                </div>
+            </div>
+        </div>
+        <div class="breed-card">
+            <div class="cow-img">
+                <img src="./assets/images/Brown-Swiss.jpg" alt="" />
+            </div>
+            <div class="container-card">
+                <div class="empty-card"></div>
+                <div class="text-element">
+                    <div class="icons-elements">
+                        <span class="material-symbols-rounded">
+                            arrow_drop_up
+                        </span>
+                        <p><span> 16%</span> Litres</p>
+                    </div>
+                    <p>Brown-Swiss</p>
+                    <h6>Health Status: <span>Healthy</span></h6>
+                </div>
+            </div>
+        </div>
+        <div class="breed-card">
+            <div class="cow-img">
+                <img src="./assets/images/Jersey.jpg" alt="" />
+            </div>
+            <div class="container-card">
+                <div class="empty-card"></div>
+                <div class="text-element">
+                    <div class="icons-elements">
+                        <span class="material-symbols-rounded">
+                            arrow_drop_up
+                        </span>
+                        <p><span> 16%</span> Litres</p>
+                    </div>
+                    <p>Jersey</p>
+                    <h6>Health Status: <span>Healthy</span></h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
 }
 
 function InformationPage() {
